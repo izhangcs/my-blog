eabb36238de35d308126305cc1e9e552082286b2
zhangcs <1114238502@qq.com>
2019-11-18 13:08:50 +0800

    修改

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.12
 require (
 	github.com/gin-gonic/gin v1.4.0
 	github.com/shirou/gopsutil v2.19.10+incompatible
+	github.com/spf13/pflag v1.0.5
 )
--- main.go
+++ main.go
@@ -8,9 +8,20 @@ import (
 	"zhangcs/blog/router"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+)
+
+var (
+	cfg = pflag.StringP("config", "c", "", "配置文件路径")
 )
 
 func main() {
+	// 初始化配置
+	pflag.Parse()
+
+	// 初始化配置
+
+	//创建gin
 	g := gin.New()
 
 	middlwares := []gin.HandlerFunc{}
